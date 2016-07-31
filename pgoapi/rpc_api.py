41a4b437471e4ff17f24689b0f8b6935c4598c8f
Jorge Mozzino <jorgemozzino@gmail.com>
2016-07-31 16:36:42 -0300

    Add timeout to requests

--- pgoapi/rpc_api.py
+++ pgoapi/rpc_api.py
@@ -89,7 +89,7 @@ class RpcApi:
 
         request_proto_serialized = request_proto_plain.SerializeToString()
         try:
-            http_response = self._session.post(endpoint, data=request_proto_serialized)
+            http_response = self._session.post(endpoint, data=request_proto_serialized, timeout=5)
         except requests.exceptions.ConnectionError as e:
             raise ServerBusyOrOfflineException
 
